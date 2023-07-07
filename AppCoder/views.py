be048137956e0801b4919efaf488452f2d1078c2
Ezequiel Gustavo De Vita <ezequieldev@hotmail.com>
2023-07-07 10:08:30 -0300

    puntoseguro, models listos

---
+++ AppCoder/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from AppCoder.models import Curso
+from django.http import HttpResponse
+# Create your views here.
+
+def curso(self):
+    curso = Curso(nombre="Desarrollo web", camada="19881")
+    curso.save()
+    
+    documentoDeTexto = f"-----> Curso:{curso.nombre} Camada: {curso.camada}"
+    return HttpResponse(documentoDeTexto)
